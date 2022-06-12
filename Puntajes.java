de68bc0377f3932bab4c5a961fd2b552c056042b
EC2 Default User <ec2-user@ip-172-31-81-49.ec2.internal>
2022-06-12 03:32:04 +0000

    agregue la 1 clase puntajes

--- Puntajes.java
+++ Puntajes.java
@@ -1,11 +1,12 @@
-public class Puntajes{	
+public class Puntajes{
+	//Atributos
 	public String jugador;
 	public int puntaje;
 
 	
-	public Puntajes (String jugador, String juego, int puntaje) {
-	this.jugador = jugador;
-	nombrejuego.nombrejuego();
-	this.puntaje = puntaje + newpuntaje;
+	public Puntajes (String _jugador, int _puntaje) {
+		//se usa _ para no escribir this. ya que java asi reconoce que _ jugador es diferente a jugador de los atributos
+	jugador = _jugador;
+	puntaje = _puntaje;
 	}
 }
