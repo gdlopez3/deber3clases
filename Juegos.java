6e18ebb3f9722ad8e332481034be0fd0a43dd5c7
EC2 Default User <ec2-user@ip-172-31-81-49.ec2.internal>
2022-06-12 03:20:47 +0000

    agregue la 1era calse Juegos

---
+++ Juegos.java
@@ -0,0 +1,13 @@
+public class Juegos{	
+	public String nombrejuego;
+	public int niveljuego;
+
+public static void main (String [] args){
+	Juegos juego1 = new Juegos();
+	
+	juego1.nombrejuego = ("FIFA");
+	juego1.niveljuego = (1);
+	
+	System.out.println (juego1.nombrejuego + juego1.niveljuego);
+}
+}
