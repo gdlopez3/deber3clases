ff24b2edcd7c993a60dfac99b55aa5b7fce34f39
EC2 Default User <ec2-user@ip-172-31-81-49.ec2.internal>
2022-06-12 03:23:48 +0000

    agregue la segunda clase Marcador

---
+++ Marcador.java
@@ -0,0 +1,12 @@
+public class Marcador{	
+	public int tiempojuego;
+	public int intentos;
+	public int resultadototal;
+
+	
+	public void Marcador (int tiempojuego, int intentos, int resultadototal) {
+	this.tiempojuego = tiempojuego + newtiempojuego;
+	this.intentos = intentos;
+	this.resultadototal = puntaje * intentos/ tiempojuego;
+	}
+}
