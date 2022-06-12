4e41c558de7fdd27ad9bbeea1ec6309d5830f87a
EC2 Default User <ec2-user@ip-172-31-81-49.ec2.internal>
2022-06-12 03:30:54 +0000

    agregue la 3 clase juegos

--- Juegos.java
+++ Juegos.java
@@ -1,13 +1,13 @@
+//Creacion de la clase, se pone nombre y el tipo de datos que van a tener los atributos
 public class Juegos{	
 	public String nombrejuego;
 	public int niveljuego;
 
-public static void main (String [] args){
-	Juegos juego1 = new Juegos();
-	
-	juego1.nombrejuego = ("FIFA");
-	juego1.niveljuego = (1);
-	
-	System.out.println (juego1.nombrejuego + juego1.niveljuego);
+//Metodos, aqui construyo el metodo de la clase Juegos, y el constructor, osea la información que va a tener ese metodo.
+//se usa _ antes del nombrede la variable para que sepa q ese valor va a ser agregado en otro momento.
+// va solo con public xq no devuelve nada.
+public Juegos (String _nombrejuego, int _niveljuego){
+	nombrejuego = _nombrejuego;
+	niveljuego = _niveljuego; 
 }
 }
